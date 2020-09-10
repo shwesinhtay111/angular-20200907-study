4eb76aa6da5a367e2650ca0d9e0c6517f9835674
ShweSinHtay <shwesin@isgm2.com>
2020-09-10 19:52:33 +0630

    spring boot connect

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -9,14 +10,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent implements OnInit {
   message = 'Some welcome message';
+  welcomeMessageFromService: string;
   name = '';
   // ActivateRoute
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private service: WelcomeDataService) { }
 
   ngOnInit() {
     console.log(this.message);
     console.log(this.route.snapshot.params.name);
     this.name = this.route.snapshot.params.name;
+  }
+  getWelcomeMessage() {
+    // console.log(' Welcome message ' );
+    console.log(this.service.executeHelloWorldBeanService());
+    // can subscribe many times, check in chrome Inspect->Network
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response)
+    );
+
+    console.log('last line of getWelcomeMessage');
+  }
+  handleSuccessfulResponse(response) {
+    // console.log(response);
+    // console.log(response.message);
+    this.welcomeMessageFromService = response.message;
   }
 
 }
